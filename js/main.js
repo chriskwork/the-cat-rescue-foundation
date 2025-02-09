d3f07eebccd120b010c0d97300080e96b9e9cc70
chris k <yovirinc@gmail.com>
2025-02-09 12:33:28 +0100

    eventlistener click add->toggle

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@ const menus = document.querySelectorAll(".has-drop-menu");
 menus.forEach((menu) => {
     menu.addEventListener("click", (e) => {
         const subMenu = menu.querySelector(".drop-menu");
-        subMenu.classList.add("show");
+        subMenu.classList.toggle("show");
     });
 });
 
